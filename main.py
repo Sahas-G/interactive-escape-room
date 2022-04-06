d5992be13a18fa1f1bfa01c39aa3d815c0a361dc
Sahas Gembali <sahasg96@gmail.com>
2022-04-05 22:11:58 -0400

    added right click functionality

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class keyboardController:
         "pan_right": 'right',
         "pan_left": 'left',
         "enter": 'enter',
-        "click": 'q'
+        "click": 'q',
+        "rClick": 'z'
     }
 
 # False--> Not being pressed, True--> Being Pressed
@@ -39,8 +40,12 @@ class keyboardController:
         'i': False,
         'j': False,
         't': False,
-        'q': False
+        'q': False,
+        'z': False
     }
+
+    navStates = ['forward', 'backward', 'left', 'right']
+    panStates = ['pan_up', 'pan_down', 'pan_left', 'pan_right']
 
     def startKey(self, action):
         pyautogui.keyDown(self.keyMap[action])
@@ -55,6 +60,27 @@ class keyboardController:
         pyautogui.press(self.keyMap[action])
         self.keyState[self.keyMap[action]] = False
 
+    def processMovement(self, action, mode='NAV'):
+        #Look at the state, if the action is new, turn off everything else, and turn on new one.
+        # Mode = NAV or PAN
+
+        if mode == 'NAV':
+            scope = self.navStates
+        else:
+            scope = self.panStates
+
+        if action == 'neutral':
+            for item in scope:
+                self.stopKey(item)
+
+        else:
+            for nav in scope:
+                if action!= nav:
+                    self.stopKey(action)
+                else:
+                    self.startKey(action)
+
+
 if __name__ == '__main__':
     print('Start of Code')
 
@@ -65,12 +91,14 @@ if __name__ == '__main__':
     #Initialise Speech Recognition
     # Create Class Objects
 
+    time.sleep(5)
+
     while True:
 
-        # continuous = ["forward", "backward", "right", "left", "pan_up", "pan_down", "pan_right", "pan_left"]
+        # continuous = ["forward", "backward", "right", "left", "pan_down", "pan_up", "pan_right", "pan_left"]
         # for item in continuous:
         #     kb.startKey(item)
-        #     time.sleep(1)
+        #     time.sleep(0.5)
         #     kb.stopKey(item)
         #
         # # for other_item in ["inventory", "diary", 'flashlight', "pause"]:
@@ -79,5 +107,11 @@ if __name__ == '__main__':
     #Step1: Look for speech/ gesture Trigger
     #Step2: If there is a trigger Validate the input can be used in the game state
     #Step3: If valid trigger --> Push to keyboard & update game state
+
+
+        pyautogui.moveTo(100, 200)
+        time.sleep(3)
+        pyautogui.moveTo(200, 100)
+        time.sleep(3)
 
 
