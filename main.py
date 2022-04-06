f91b61277b43328a453381145be6077baa71b631
Sahas Gembali <sahasg96@gmail.com>
2022-04-05 22:43:24 -0400

    tiny bug in keyboard class, processMovement

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ class keyboardController:
                 if action is not nav:
                     self.stopKey(action)
                 else:
-                    if not self.keyState[action]:
+                    if not self.keyState[self.keyMap[action]]:
                         self.startKey(action)
 
 
